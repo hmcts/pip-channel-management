2912e22d156f4a2ddd2cf30d8fd8712cbb648d57
Josh Blackmoor <joshblackmoor01@icloud.com>
2022-08-19 17:08:55 +0100

    Updated web client

--- src/main/java/uk/gov/hmcts/reform/pip/channel/management/config/WebClientConfig.java
+++ src/main/java/uk/gov/hmcts/reform/pip/channel/management/config/WebClientConfig.java
@@ -3,8 +3,13 @@ package uk.gov.hmcts.reform.pip.channel.management.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -15,18 +20,35 @@ public class WebClientConfig {
 
     @Bean
     @Profile("!dev")
-    public WebClient webClient(ClientRegistrationRepository clientRegistrations,
-                        OAuth2AuthorizedClientRepository authorizedClients) {
-        ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2 =
-            new ServletOAuth2AuthorizedClientExchangeFilterFunction(
-                clientRegistrations, authorizedClients);
-        oauth2.setDefaultClientRegistrationId("accountManagementApi");
-        return WebClient.builder().apply(oauth2.oauth2Configuration()).build();
+    public OAuth2AuthorizedClientManager authorizedClientManager(ClientRegistrationRepository clients) {
+        OAuth2AuthorizedClientService service = new InMemoryOAuth2AuthorizedClientService(clients);
+        AuthorizedClientServiceOAuth2AuthorizedClientManager manager =
+            new AuthorizedClientServiceOAuth2AuthorizedClientManager(clients, service);
+
+        OAuth2AuthorizedClientProvider authorizedClientProvider =
+            OAuth2AuthorizedClientProviderBuilder.builder()
+                .clientCredentials()
+                .build();
+
+        manager.setAuthorizedClientProvider(authorizedClientProvider);
+
+        return manager;
+    }
+
+    @Bean
+    @Profile("!dev")
+    WebClient webClient(OAuth2AuthorizedClientManager authorizedClientManager) {
+        ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2Client =
+            new ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
+        oauth2Client.setDefaultClientRegistrationId("accountManagementApi");
+        return WebClient.builder()
+            .apply(oauth2Client.oauth2Configuration())
+            .build();
     }
 
     @Bean
     @Profile("dev")
-    public WebClient webClientInsecure() {
+    WebClient webClientInsecure() {
         return WebClient.builder().build();
     }
 }
