8e9af90bd019fd8ee28efc9a8bc7bf9697b30d66
Josh Blackmoor <joshblackmoor01@icloud.com>
2022-11-08 09:43:35 +0000

    Remove duplications

--- src/main/java/uk/gov/hmcts/reform/pip/channel/management/services/artefactsummary/CrownDailyListSummaryConverter.java
+++ src/main/java/uk/gov/hmcts/reform/pip/channel/management/services/artefactsummary/CrownDailyListSummaryConverter.java
@@ -5,8 +5,9 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.pip.channel.management.models.external.datamanagement.Language;
+import uk.gov.hmcts.reform.pip.channel.management.models.external.datamanagement.ListType;
+import uk.gov.hmcts.reform.pip.channel.management.services.artefactsummary.helpers.CrimeListSummaryHelper;
 import uk.gov.hmcts.reform.pip.channel.management.services.filegeneration.helpers.DataManipulation;
-import uk.gov.hmcts.reform.pip.channel.management.services.filegeneration.helpers.GeneralHelper;
 import uk.gov.hmcts.reform.pip.channel.management.services.filegeneration.helpers.listmanipulation.CrownDailyListHelper;
 
 @Service
@@ -28,60 +29,6 @@ public class CrownDailyListSummaryConverter implements ArtefactSummaryConverter 
         CrownDailyListHelper.manipulatedCrownDailyListData(node);
         CrownDailyListHelper.findUnallocatedCasesInCrownDailyListData(node);
 
-        return this.processCrownDailyList(node);
-    }
-
-    private String processCrownDailyList(JsonNode node) {
-        StringBuilder output = new StringBuilder();
-        node.get("courtLists").forEach(courtList -> {
-            courtList.get("courtHouse").get("courtRoom").forEach(courtRoom -> {
-                courtRoom.get("session").forEach(session -> {
-                    session.get("sittings").forEach(sitting -> {
-                        sitting.get("hearing").forEach(hearing -> {
-                            hearing.get("case").forEach(hearingCase -> {
-                                output.append('\n');
-                                GeneralHelper.appendToStringBuilder(output, "Sitting at - ",
-                                                                    sitting, "time");
-                                GeneralHelper.appendToStringBuilder(output, "Case Reference - ",
-                                                                    hearingCase, "caseNumber");
-                                GeneralHelper.appendToStringBuilder(output, "Defendant Name(s) - ",
-                                                                    hearing, "defendant");
-                                GeneralHelper.appendToStringBuilder(output, "Hearing Type - ",
-                                                                    hearing, "hearingType");
-                                output.append('\n');
-                                checkCaseSequenceNo(output, sitting, hearingCase);
-                                GeneralHelper.appendToStringBuilder(output, "Prosecuting Authority - ",
-                                                                    hearing,"prosecuting_authority");
-                                if (!GeneralHelper.findAndReturnNodeText(hearingCase, "linkedCases").isEmpty()) {
-                                    GeneralHelper.appendToStringBuilder(output, "Linked Cases - ",
-                                                                        hearingCase, "linkedCases"
-                                    );
-                                }
-                                if (!GeneralHelper.findAndReturnNodeText(hearing, "listingNotes").isEmpty()) {
-                                    GeneralHelper.appendToStringBuilder(output, "Listing Notes - ",
-                                                                        hearing, "listingNotes"
-                                    );
-                                }
-                            });
-                        });
-                    });
-                });
-            });
-        });
-
-        return output.toString();
-    }
-
-    private void checkCaseSequenceNo(StringBuilder output, JsonNode sitting, JsonNode hearingCase) {
-        String caseSequenceNo = "";
-        if (!GeneralHelper.findAndReturnNodeText(hearingCase, "linkedCases")
-            .isEmpty()) {
-            caseSequenceNo = " " + GeneralHelper.findAndReturnNodeText(hearingCase,
-                                                                       "caseSequenceIndicator");
-        }
-        String formattedDuration = "Duration - "
-            + GeneralHelper.findAndReturnNodeText(sitting, "formattedDuration")
-            + caseSequenceNo;
-        output.append(formattedDuration);
+        return CrimeListSummaryHelper.processCrimeList(node, ListType.CROWN_DAILY_LIST);
     }
 }
--- src/main/java/uk/gov/hmcts/reform/pip/channel/management/services/artefactsummary/MagistratesPublicListSummaryConverter.java
+++ src/main/java/uk/gov/hmcts/reform/pip/channel/management/services/artefactsummary/MagistratesPublicListSummaryConverter.java
@@ -5,8 +5,9 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.pip.channel.management.models.external.datamanagement.Language;
+import uk.gov.hmcts.reform.pip.channel.management.models.external.datamanagement.ListType;
+import uk.gov.hmcts.reform.pip.channel.management.services.artefactsummary.helpers.CrimeListSummaryHelper;
 import uk.gov.hmcts.reform.pip.channel.management.services.filegeneration.helpers.DataManipulation;
-import uk.gov.hmcts.reform.pip.channel.management.services.filegeneration.helpers.GeneralHelper;
 import uk.gov.hmcts.reform.pip.channel.management.services.filegeneration.helpers.listmanipulation.MagistratesPublicListHelper;
 
 @Service
@@ -26,53 +27,6 @@ public class MagistratesPublicListSummaryConverter implements ArtefactSummaryCon
         DataManipulation.manipulatedDailyListData(node, Language.ENGLISH, false);
         MagistratesPublicListHelper.manipulatedMagistratesPublicListData(node);
 
-        return this.processMagistratesPublicList(node);
-    }
-
-    private String processMagistratesPublicList(JsonNode node) {
-        StringBuilder output = new StringBuilder();
-        node.get("courtLists").forEach(courtList -> {
-            courtList.get("courtHouse").get("courtRoom").forEach(courtRoom -> {
-                courtRoom.get("session").forEach(session -> {
-                    session.get("sittings").forEach(sitting -> {
-                        sitting.get("hearing").forEach(hearing -> {
-                            hearing.get("case").forEach(hearingCase -> {
-
-                                output.append('\n');
-
-                                GeneralHelper.appendToStringBuilder(output, "Sitting at - ",
-                                                                    sitting, "time");
-                                GeneralHelper.appendToStringBuilder(output, "Case Reference - ",
-                                                                    hearingCase, "caseNumber");
-                                GeneralHelper.appendToStringBuilder(output, "Defendant Name(s) - ",
-                                                                    hearing, "defendant");
-                                GeneralHelper.appendToStringBuilder(output, "Hearing Type - ",
-                                                                    hearing, "hearingType");
-                                GeneralHelper.appendToStringBuilder(output, "Prosecuting Authority - ",
-                                                                    hearing,"prosecuting_authority");
-                                output.append('\n');
-                                formatDuration(output, sitting, hearingCase);
-                                if (!GeneralHelper.findAndReturnNodeText(hearing, "listingNotes").isEmpty()) {
-                                    GeneralHelper.appendToStringBuilder(output, "Case Details - ",
-                                                                        hearing, "listingNotes"
-                                    );
-                                }
-                            });
-                        });
-                    });
-                });
-            });
-        });
-
-        return output.toString();
-    }
-
-    private void formatDuration(StringBuilder output, JsonNode sitting, JsonNode hearingCase) {
-        String caseSequenceNo = " " + GeneralHelper.findAndReturnNodeText(hearingCase,
-                                                                   "caseSequenceIndicator");
-        String formattedDuration = "Duration - "
-            + GeneralHelper.findAndReturnNodeText(sitting, "formattedDuration")
-            + caseSequenceNo;
-        output.append(formattedDuration);
+        return CrimeListSummaryHelper.processCrimeList(node, ListType.MAGISTRATES_PUBLIC_LIST);
     }
 }
