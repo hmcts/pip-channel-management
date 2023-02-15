7c5e5923ce0e657f48e8c0c3a3e2205544ca4c73
Josh Blackmoor <joshblackmoor01@icloud.com>
2023-02-15 16:55:22 +0000

    Unit tests added

--- src/test/java/uk/gov/hmcts/reform/pip/channel/management/errorhandling/GlobalExceptionHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/pip/channel/management/errorhandling/GlobalExceptionHandlerTest.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.pip.channel.management.errorhandling;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import uk.gov.hmcts.reform.pip.channel.management.errorhandling.exceptions.AuthorisedException;
 import uk.gov.hmcts.reform.pip.channel.management.errorhandling.exceptions.ChannelNotFoundException;
 import uk.gov.hmcts.reform.pip.channel.management.errorhandling.exceptions.NotFoundException;
 import uk.gov.hmcts.reform.pip.channel.management.errorhandling.exceptions.ProcessingException;
@@ -15,6 +16,7 @@ class GlobalExceptionHandlerTest {
 
     private static final String MESSAGES_MATCH = "The message should match the message passed in";
     private static final String RESPONSE_BODY_MESSAGE = "Response should contain a body";
+    private static final String STATUS_CODE_MATCH = "Status code should match";
 
     static final String TEST_MESSAGE = "This is a test message";
     private final GlobalExceptionHandler globalExceptionHandler = new GlobalExceptionHandler();
@@ -26,7 +28,7 @@ class GlobalExceptionHandlerTest {
         ResponseEntity<ExceptionResponse> responseEntity =
             globalExceptionHandler.handleChannelNotFound(channelNotFoundException);
 
-        assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode(), "Status code should be not found");
+        assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode(), STATUS_CODE_MATCH);
         assertNotNull(responseEntity.getBody(), RESPONSE_BODY_MESSAGE);
         assertEquals(TEST_MESSAGE, responseEntity.getBody().getMessage(),
                      MESSAGES_MATCH);
@@ -38,7 +40,7 @@ class GlobalExceptionHandlerTest {
 
         ResponseEntity<ExceptionResponse> responseEntity = globalExceptionHandler.handleServiceToService(exception);
 
-        assertEquals(HttpStatus.BAD_GATEWAY, responseEntity.getStatusCode(), "Status code should be bad gateway");
+        assertEquals(HttpStatus.BAD_GATEWAY, responseEntity.getStatusCode(), STATUS_CODE_MATCH);
         assertNotNull(responseEntity.getBody(), RESPONSE_BODY_MESSAGE);
         assertEquals("Request to Test service failed due to: " +  TEST_MESSAGE,
                      responseEntity.getBody().getMessage(), MESSAGES_MATCH);
@@ -50,7 +52,7 @@ class GlobalExceptionHandlerTest {
 
         ResponseEntity<ExceptionResponse> responseEntity = globalExceptionHandler.handleNotFound(exception);
 
-        assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode(), "Status code should be not found");
+        assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode(), STATUS_CODE_MATCH);
         assertNotNull(responseEntity.getBody(), RESPONSE_BODY_MESSAGE);
         assertEquals(TEST_MESSAGE, responseEntity.getBody().getMessage(),
                      MESSAGES_MATCH);
@@ -63,7 +65,20 @@ class GlobalExceptionHandlerTest {
         ResponseEntity<ExceptionResponse> responseEntity = globalExceptionHandler.handleProcessingException(exception);
 
         assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, responseEntity.getStatusCode(),
-                     "Status code should be not found");
+                     STATUS_CODE_MATCH);
+        assertNotNull(responseEntity.getBody(), RESPONSE_BODY_MESSAGE);
+        assertEquals(TEST_MESSAGE, responseEntity.getBody().getMessage(),
+                     MESSAGES_MATCH);
+    }
+
+    @Test
+    void testHandleAuthorisedException() {
+        AuthorisedException exception = new AuthorisedException(TEST_MESSAGE);
+
+        ResponseEntity<ExceptionResponse> responseEntity = globalExceptionHandler.handleAuthorisedException(exception);
+
+        assertEquals(HttpStatus.UNAUTHORIZED, responseEntity.getStatusCode(),
+                     STATUS_CODE_MATCH);
         assertNotNull(responseEntity.getBody(), RESPONSE_BODY_MESSAGE);
         assertEquals(TEST_MESSAGE, responseEntity.getBody().getMessage(),
                      MESSAGES_MATCH);
