49d52bc55cc76d370a3539b2ba180c4db975a580
Kian Kwa <kian.kwa@justice.gov.uk>
2023-06-30 15:43:24 +0100

    Add log for testing

--- src/main/java/uk/gov/hmcts/reform/pip/channel/management/config/AzureBlobConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/pip/channel/management/config/AzureBlobConfiguration.java
@@ -23,11 +23,14 @@ public class AzureBlobConfiguration {
     @Bean
     public BlobContainerClient blobContainerClient(AzureBlobConfigurationProperties azureBlobConfigurationProperties) {
         if (managedIdentityClientId.isEmpty()) {
+            System.out.println("*****Use connection string to connect to Azure blob");
+
             return new BlobContainerClientBuilder()
                 .connectionString(azureBlobConfigurationProperties.getConnectionString())
                 .containerName(azureBlobConfigurationProperties.getContainerName())
                 .buildClient();
         }
+        System.out.println("*****Use MI to connect to Azure blob");
 
         DefaultAzureCredential defaultCredential = new DefaultAzureCredentialBuilder()
             .tenantId(tenantId)
