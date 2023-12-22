48c8d8c60c077e2ac5b5ed1f6c939dfdf22c755d
Kian Kwa <kian.kwa@justice.gov.uk>
2023-12-22 16:32:35 +0000

    Fix checkstyle

--- src/test/java/uk/gov/hmcts/reform/pip/channel/management/services/helpers/LocationHelperTest.java
+++ src/test/java/uk/gov/hmcts/reform/pip/channel/management/services/helpers/LocationHelperTest.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.pip.channel.management.services.helpers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
