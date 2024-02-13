78d96856438a9183a4dfb9144dcb5e96a545cc8e
Kian Kwa <kian.kwa@justice.gov.uk>
2024-02-13 18:19:48 +0000

    Add security to Swagger API

--- src/main/java/uk/gov/hmcts/reform/pip/channel/management/controllers/PublicationManagementController.java
+++ src/main/java/uk/gov/hmcts/reform/pip/channel/management/controllers/PublicationManagementController.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.pip.channel.management.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,10 +27,12 @@ import java.util.UUID;
 @RestController
 @Tag(name = "Controller to handle the generation and retrieval of Excel/PDF publications and artefact summaries")
 @RequestMapping("/publication")
+@ApiResponse(responseCode = "401", description = "Invalid access credential")
+@ApiResponse(responseCode = "403", description = "User has not been authorized")
 @IsAdmin
+@SecurityRequirement(name = "Bearer authentication")
 public class PublicationManagementController {
 
-    private static final String UNAUTHORIZED_DESCRIPTION = "User has not been authorized";
     private static final String NOT_FOUND_DESCRIPTION = "No artefact found";
     private final PublicationManagementService publicationManagementService;
 
@@ -37,7 +40,6 @@ public class PublicationManagementController {
     private static final String ACCEPTED_CODE = "202";
     private static final String NO_CONTENT_CODE = "204";
     private static final String NOT_FOUND_CODE = "404";
-    private static final String NO_AUTH_CODE = "403";
     private static final String PAYLOAD_TOO_LARGE_CODE = "413";
     private static final String INTERNAL_ERROR_CODE = "500";
 
@@ -48,7 +50,6 @@ public class PublicationManagementController {
 
     @ApiResponse(responseCode = OK_CODE, description = "Artefact summary string returned")
     @ApiResponse(responseCode = NOT_FOUND_CODE, description = NOT_FOUND_DESCRIPTION)
-    @ApiResponse(responseCode = NO_AUTH_CODE, description = UNAUTHORIZED_DESCRIPTION)
     @ApiResponse(responseCode = INTERNAL_ERROR_CODE, description = "Cannot process the artefact")
     @Operation(summary = "Takes in an artefact ID and returns an artefact summary")
     @GetMapping("/summary/{artefactId}")
@@ -58,7 +59,6 @@ public class PublicationManagementController {
 
     @ApiResponse(responseCode = ACCEPTED_CODE, description = "Request to generate files accepted")
     @ApiResponse(responseCode = NOT_FOUND_CODE, description = NOT_FOUND_DESCRIPTION)
-    @ApiResponse(responseCode = NO_AUTH_CODE, description = UNAUTHORIZED_DESCRIPTION)
     @ApiResponse(responseCode = INTERNAL_ERROR_CODE, description = "Cannot process the artefact")
     @Operation(summary = "Takes in an artefact ID and generates/stores a publication file")
     @PostMapping("/{artefactId}")
@@ -69,7 +69,6 @@ public class PublicationManagementController {
 
     @ApiResponse(responseCode = OK_CODE, description = "PDF or Excel file for an artefact returned successfully")
     @ApiResponse(responseCode = NOT_FOUND_CODE, description = NOT_FOUND_DESCRIPTION)
-    @ApiResponse(responseCode = NO_AUTH_CODE, description = UNAUTHORIZED_DESCRIPTION)
     @ApiResponse(responseCode = PAYLOAD_TOO_LARGE_CODE, description = "File size too large")
     @Operation(summary = "Takes in an artefact ID and returns the stored PDF or Excel file ")
     @GetMapping("/v2/{artefactId}")
@@ -88,7 +87,6 @@ public class PublicationManagementController {
     }
 
     @ApiResponse(responseCode = NO_CONTENT_CODE, description = "The files have been deleted")
-    @ApiResponse(responseCode = NO_AUTH_CODE, description = UNAUTHORIZED_DESCRIPTION)
     @Operation(summary = "Takes in an artefact ID and delete all publication files associated with the artefact")
     @DeleteMapping("/v2/{artefactId}")
     public ResponseEntity<Void> deleteFiles(
