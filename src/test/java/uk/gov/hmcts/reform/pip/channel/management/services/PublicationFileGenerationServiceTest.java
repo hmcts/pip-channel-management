8ccbf2e44650eb474803b78ae1ab2557bb0f4b9e
Kian Kwa <kian.kwa@justice.gov.uk>
2024-03-19 16:53:06 +0000

    Fix checkstyle issue

--- src/test/java/uk/gov/hmcts/reform/pip/channel/management/services/PublicationFileGenerationServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/pip/channel/management/services/PublicationFileGenerationServiceTest.java
@@ -28,8 +28,8 @@ import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ActiveProfiles(profiles = "test")
 @SpringBootTest(classes = {Application.class})
