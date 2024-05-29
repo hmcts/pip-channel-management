84eace326a955851ed92ab52f36cc02a09863f1b
Paul Hudson <paul.hudson3@justice.gov.uk>
2024-05-29 11:14:26 +0100

    PUB-2454 - Add PDF and email summary generation for IAC Daily List

--- src/test/java/uk/gov/hmcts/reform/pip/channel/management/services/filegeneration/IacDailyListFileConverterTest.java
+++ src/test/java/uk/gov/hmcts/reform/pip/channel/management/services/filegeneration/IacDailyListFileConverterTest.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.pip.channel.management.services.filegeneration;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.io.IOUtils;
@@ -9,14 +8,15 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.util.Map;
-import java.util.Objects;
+
 
 @ActiveProfiles("test")
 class IacDailyListFileConverterTest {
@@ -33,183 +33,186 @@ class IacDailyListFileConverterTest {
 
     @BeforeAll
     public static void beforeAll() throws IOException {
-        Map<String, String> metaData = Map.of("contentDate", "02 October 2022",
-                                              "language", "ENGLISH",
-                                              "provenance", "MANUAL_UPLOAD",
-                                              "locationName", "Location Name",
-                                              "listType", "IAC_DAILY_LIST"
-        );
-        Map<String, Object> language = handleLanguage();
         JsonNode input = getInput("/mocks/iacDailyList.json");
 
         String result = CONVERTER.convert(input, metaData, language);
         doc = Jsoup.parse(result);
     }
 
-    @Test
-    void testSuccessfulConversionMetadata() {
-        SoftAssertions softly = new SoftAssertions();
+    @ParameterizedTest
+    @EnumSource(value = ListType.class, names = {"SJP_PUBLIC_LIST", "SJP_DELTA_PUBLIC_LIST"})
+    void testSuccessfulConversion(ListType listType) throws IOException {
+        Map<String, Object> language = TestUtils.getLanguageResources(listType, "en");
+        Map<String, String> metaData = Map.of("contentDate", "1 July 2022",
+                                              "language", "ENGLISH",
+                                              "listType", listType.name()
+        );
 
-        softly.assertThat(doc.getElementsByTag("h2"))
-            .as("Incorrect h2 element")
-            .hasSize(2)
-            .extracting(Element::text)
-            .containsExactly(
-                "First-tier Tribunal: Immigration and Asylum Chamber",
-                "Location Name Daily List"
-            );
+        @Test
+        void testSuccessfulConversionMetadata () {
+            SoftAssertions softly = new SoftAssertions();
 
-        softly.assertThat(doc.getElementsByClass("header").get(0).getElementsByTag("p"))
-            .as("Incorrect p elements")
-            .isNotEmpty()
-            .extracting(Element::text)
-            .contains(
-                "List for 02 October 2022",
-                "Last updated 20 October 2022 at 9pm"
-            );
+            softly.assertThat(doc.getElementsByTag("h2"))
+                .as("Incorrect h2 element")
+                .hasSize(2)
+                .extracting(Element::text)
+                .containsExactly(
+                    "First-tier Tribunal: Immigration and Asylum Chamber",
+                    "Location Name Daily List"
+                );
 
-        softly.assertThat(doc.getElementsByTag("section").get(0).getElementsByTag("p").get(0))
-            .as("Incorrect data source")
-            .extracting(Element::text)
-            .isEqualTo("Data Source: MANUAL_UPLOAD");
+            softly.assertThat(doc.getElementsByClass("header").get(0).getElementsByTag("p"))
+                .as("Incorrect p elements")
+                .isNotEmpty()
+                .extracting(Element::text)
+                .contains(
+                    "List for 02 October 2022",
+                    "Last updated 20 October 2022 at 9pm"
+                );
 
-        softly.assertAll();
-    }
+            softly.assertThat(doc.getElementsByTag("section").get(0).getElementsByTag("p").get(0))
+                .as("Incorrect data source")
+                .extracting(Element::text)
+                .isEqualTo("Data Source: MANUAL_UPLOAD");
 
-    @Test
-    void testSuccessfulConversionBailList() {
-        SoftAssertions softly = new SoftAssertions();
-
-        softly.assertThat(doc.getElementsByTag("h1").get(0))
-            .as("Incorrect h1 element")
-            .extracting(Element::text)
-            .isEqualTo("Bail List");
-
-        softly.assertThat(doc.getElementsByClass("govuk-accordion").get(0).getElementsByTag("h3").get(0))
-            .as("Incorrect room name element")
-            .extracting(Element::text)
-            .isEqualTo("Court Room A, Before Judge Test Name, Magistrate Test Name");
-
-        softly.assertThat(doc.getElementsByClass("govuk-table__head").get(0).getElementsByTag("th"))
-            .as("Incorrect table headers")
-            .hasSize(7)
-            .extracting(Element::text)
-            .containsExactly(
-                "Start Time",
-                "Case Ref",
-                "Appellant/Applicant",
-                "Respondent",
-                "Interpreter Language",
-                "Hearing Channel",
-                "Hearing Type"
-            );
-
-        softly.assertThat(doc.getElementsByClass(GOVUK_TABLE_BODY).get(0).getElementsByTag("td"))
-            .as(TABLE_ROW_ERROR)
-            .hasSize(7)
-            .extracting(Element::text)
-            .contains(
-                "9:00pm",
-                CASE_REF,
-                "Surname Rep: Mr Individual Forenames Individual Surname",
-                RESPONDENT,
-                "French",
-                "Teams, Attended",
-                "Directions"
-            );
-
-        softly.assertThat(doc.getElementsByClass(GOVUK_TABLE_BODY).get(1).getElementsByTag("td"))
-            .as(TABLE_ROW_ERROR)
-            .hasSize(7)
-            .extracting(Element::text)
-            .contains(
-                "9:00pm",
-                CASE_REF,
-                "Organisation Name Rep: Organisation Name",
-                "Organisation Name",
-                "French",
-                "VIDEO HEARING",
-                ""
-            );
-
-        softly.assertAll();
-    }
-
-    @Test
-    void testSuccessfulConversionNonBailList() {
-        SoftAssertions softly = new SoftAssertions();
-
-        softly.assertThat(doc.getElementsByTag("h1").get(1))
-            .as("Incorrect h1 element")
-            .extracting(Element::text)
-            .isEqualTo("Non Bail List");
-
-        softly.assertThat(doc.getElementsByClass("govuk-accordion").get(1).getElementsByTag("h3").get(0))
-            .as("Incorrect room name element")
-            .extracting(Element::text)
-            .isEqualTo("Hearing Room: Court Room B");
-
-        softly.assertThat(doc.getElementsByClass("govuk-table__head").get(2).getElementsByTag("th"))
-            .as("Incorrect table headers")
-            .hasSize(7)
-            .extracting(Element::text)
-            .containsExactly(
-                "Start Time",
-                "Case Ref",
-                "Appellant/Applicant",
-                "Respondent",
-                "Interpreter Language",
-                "Hearing Channel",
-                "Hearing Type"
-            );
-
-        softly.assertThat(doc.getElementsByClass(GOVUK_TABLE_BODY).get(2)
-                              .getElementsByClass("govuk-table__row").get(0).getElementsByTag("td"))
-            .as(TABLE_ROW_ERROR)
-            .hasSize(7)
-            .extracting(Element::text)
-            .contains(
-                "9:20pm",
-                CASE_REF,
-                "Surname Rep: No Representative",
-                RESPONDENT,
-                "",
-                "Teams, Attended",
-                ""
-            );
-
-        softly.assertThat(doc.getElementsByClass(GOVUK_TABLE_BODY).get(2)
-                              .getElementsByClass("govuk-table__row").get(1).getElementsByTag("td"))
-            .as(TABLE_ROW_ERROR)
-            .hasSize(7)
-            .extracting(Element::text)
-            .contains(
-                "9:20pm",
-                CASE_REF,
-                "Surname Rep: Mr Individual Forenames Individual Surname",
-                RESPONDENT,
-                "",
-                "VIDEO HEARING",
-                ""
-            );
-
-        softly.assertAll();
-    }
-
-    private static JsonNode getInput(String resourcePath) throws IOException {
-        try (InputStream inputStream = IacDailyListFileConverterTest.class.getResourceAsStream(resourcePath)) {
-            String inputRaw = IOUtils.toString(inputStream, Charset.defaultCharset());
-            return new ObjectMapper().readTree(inputRaw);
+            softly.assertAll();
         }
-    }
 
-    private static Map<String, Object> handleLanguage() throws IOException {
-        try (InputStream languageFile = Thread.currentThread()
-            .getContextClassLoader().getResourceAsStream("templates/languages/en/iacDailyList.json")) {
-            return new ObjectMapper().readValue(
-                Objects.requireNonNull(languageFile).readAllBytes(), new TypeReference<>() {
-                });
+        @Test
+        void testSuccessfulConversionBailList () {
+            SoftAssertions softly = new SoftAssertions();
+
+            softly.assertThat(doc.getElementsByTag("h1").get(0))
+                .as("Incorrect h1 element")
+                .extracting(Element::text)
+                .isEqualTo("Bail List");
+
+            softly.assertThat(doc.getElementsByClass("govuk-accordion").get(0).getElementsByTag("h3").get(0))
+                .as("Incorrect room name element")
+                .extracting(Element::text)
+                .isEqualTo("Court Room A, Before Judge Test Name, Magistrate Test Name");
+
+            softly.assertThat(doc.getElementsByClass("govuk-table__head").get(0).getElementsByTag("th"))
+                .as("Incorrect table headers")
+                .hasSize(7)
+                .extracting(Element::text)
+                .containsExactly(
+                    "Start Time",
+                    "Case Ref",
+                    "Appellant/Applicant",
+                    "Respondent",
+                    "Interpreter Language",
+                    "Hearing Channel",
+                    "Hearing Type"
+                );
+
+            softly.assertThat(doc.getElementsByClass(GOVUK_TABLE_BODY).get(0).getElementsByTag("td"))
+                .as(TABLE_ROW_ERROR)
+                .hasSize(7)
+                .extracting(Element::text)
+                .contains(
+                    "9:00pm",
+                    CASE_REF,
+                    "Surname Rep: Mr Individual Forenames Individual Surname",
+                    RESPONDENT,
+                    "French",
+                    "Teams, Attended",
+                    "Directions"
+                );
+
+            softly.assertThat(doc.getElementsByClass(GOVUK_TABLE_BODY).get(1).getElementsByTag("td"))
+                .as(TABLE_ROW_ERROR)
+                .hasSize(7)
+                .extracting(Element::text)
+                .contains(
+                    "9:00pm",
+                    CASE_REF,
+                    "Organisation Name Rep: Organisation Name",
+                    "Organisation Name",
+                    "French",
+                    "VIDEO HEARING",
+                    ""
+                );
+
+            softly.assertAll();
         }
-    }
 
+        @Test
+        void testSuccessfulConversionNonBailList () {
+            SoftAssertions softly = new SoftAssertions();
+
+            softly.assertThat(doc.getElementsByTag("h1").get(1))
+                .as("Incorrect h1 element")
+                .extracting(Element::text)
+                .isEqualTo("Non Bail List");
+
+            softly.assertThat(doc.getElementsByClass("govuk-accordion").get(1).getElementsByTag("h3").get(0))
+                .as("Incorrect room name element")
+                .extracting(Element::text)
+                .isEqualTo("Hearing Room: Court Room B");
+
+            softly.assertThat(doc.getElementsByClass("govuk-table__head").get(2).getElementsByTag("th"))
+                .as("Incorrect table headers")
+                .hasSize(7)
+                .extracting(Element::text)
+                .containsExactly(
+                    "Start Time",
+                    "Case Ref",
+                    "Appellant/Applicant",
+                    "Respondent",
+                    "Interpreter Language",
+                    "Hearing Channel",
+                    "Hearing Type"
+                );
+
+            softly.assertThat(doc.getElementsByClass(GOVUK_TABLE_BODY).get(2)
+                                  .getElementsByClass("govuk-table__row").get(0).getElementsByTag("td"))
+                .as(TABLE_ROW_ERROR)
+                .hasSize(7)
+                .extracting(Element::text)
+                .contains(
+                    "9:20pm",
+                    CASE_REF,
+                    "Surname Rep: No Representative",
+                    RESPONDENT,
+                    "",
+                    "Teams, Attended",
+                    ""
+                );
+
+            softly.assertThat(doc.getElementsByClass(GOVUK_TABLE_BODY).get(2)
+                                  .getElementsByClass("govuk-table__row").get(1).getElementsByTag("td"))
+                .as(TABLE_ROW_ERROR)
+                .hasSize(7)
+                .extracting(Element::text)
+                .contains(
+                    "9:20pm",
+                    CASE_REF,
+                    "Surname Rep: Mr Individual Forenames Individual Surname",
+                    RESPONDENT,
+                    "",
+                    "VIDEO HEARING",
+                    ""
+                );
+
+            softly.assertAll();
+        }
+
+        private static JsonNode getInput (String resourcePath) throws IOException {
+            try (InputStream inputStream = IacDailyListFileConverterTest.class.getResourceAsStream(resourcePath)) {
+                String inputRaw = IOUtils.toString(inputStream, Charset.defaultCharset());
+                return new ObjectMapper().readTree(inputRaw);
+            }
+        }
+
+        private static Map<String, Object> handleLanguage () throws IOException {
+            try (InputStream languageFile = Thread.currentThread()
+                .getContextClassLoader().getResourceAsStream("templates/languages/en/iacDailyList.json")) {
+                return new ObjectMapper().readValue(
+                    Objects.requireNonNull(languageFile).readAllBytes(), new TypeReference<>() {
+                    });
+            }
+        }
+
+    }
 }
