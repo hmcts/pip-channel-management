fdf263e3e5b15ab5209e8224087efadc9f1ea1b8
Kian Kwa <kian.kwa@justice.gov.uk>
2024-06-21 12:25:44 +0100

    ed checkstyle

--- src/main/java/uk/gov/hmcts/reform/pip/channel/management/services/artefactsummary/EtFortnightlyPressListSummaryData.java
+++ src/main/java/uk/gov/hmcts/reform/pip/channel/management/services/artefactsummary/EtFortnightlyPressListSummaryData.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.pip.channel.management.services.artefactsummary;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.collect.ImmutableMap;
 import org.springframework.stereotype.Service;
